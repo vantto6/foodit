38a6685c4b20b9ce1d08823385724c7e640e4a5c
leemi <leemi@DESKTOP-EHRC860>
2023-05-15 16:18:14 +0900

    장바구니 작업중

--- Foodit/src/main/java/com/basket/BasketDAO.java
+++ Foodit/src/main/java/com/basket/BasketDAO.java
@@ -1,5 +1,7 @@
 package com.basket;
 
 public class BasketDAO {
+	
+	
 
 }
--- Foodit/src/main/java/com/basket/BasketDTO.java
+++ Foodit/src/main/java/com/basket/BasketDTO.java
@@ -1,5 +1,34 @@
 package com.basket;
 
 public class BasketDTO {
+	private int basketNo;
+	private int basketCnt;
+	private String memberId;
+	private int itemNo;
+	
+	public int getBasketNo() {
+		return basketNo;
+	}
+	public void setBasketNo(int basketNo) {
+		this.basketNo = basketNo;
+	}
+	public int getBasketCnt() {
+		return basketCnt;
+	}
+	public void setBasketCnt(int basketCnt) {
+		this.basketCnt = basketCnt;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public int getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(int itemNo) {
+		this.itemNo = itemNo;
+	}
 
 }
